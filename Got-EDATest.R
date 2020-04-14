e8da6d5bb8b59d16cf8881b3a8f2273c4d7cc609
Sumitra <sumitrabinu@gmail.com>
2020-04-14 23:00:02 +0530

    Added statements for generating 5 point summary of mtcars

--- Got-EDATest.R
+++ Got-EDATest.R
@@ -1,2 +1,8 @@
 print("This file was created within Sumi's RStudio")
 print("And now it lives on Sumitra Binu's GitHub EDATest repository")
+k<-read.csv("mtcars.csv")
+load(datasets)
+data()
+View(mtcars)
+summary(mtcars)
+names(mtcars)
